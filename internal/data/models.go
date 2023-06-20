bc7ae3ed4c285f9b6c874693fcc008a8f35b2d13
ammo <solidifyarmor@gmail.com>
2023-06-20 08:55:05 +0630

    token model

--- internal/data/models.go
+++ internal/data/models.go
@@ -13,11 +13,13 @@ var (
 type Models struct {
 	Movies MovieModel
 	Users  UserModel
+	Tokens TokenModal
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
 		Users:  UserModel{DB: db},
+		Tokens: TokenModal{DB: db},
 	}
 }
