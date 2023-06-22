8ae0e2ea80817254f470eae6bcff4cc9769c2824
ammo <solidifyarmor@gmail.com>
2023-06-22 11:31:53 +0630

    preflight cors

---
+++ cmd/examples/cors/preflight/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+const html = `
+<!Doctype html>
+<html>
+<head><meta charset="UTF-8"></head>
+<body>
+<h1>Simple CORS</h1>
+<div id="output" />
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+	fetch("http://localhost:4000/v1/tokens/authentication", {
+		method: "POST",
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			email: 'hey@agmmtoo.me',
+			password: 'passwor'
+		})
+	}).then(function(response) {
+		response.text().then(function(text) {
+			document.getElementById("output").innerHTML = text;
+		});
+	}, function(err) {
+		document.getElementById("output").innerHTML = err;
+	});
+});
+</script>
+</body>
+</html>`
+
+func main() {
+	addr := flag.String("addr", ":9000", "Server address")
+	flag.Parse()
+	log.Printf("starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(html))
+	}))
+	log.Fatal(err)
+}
