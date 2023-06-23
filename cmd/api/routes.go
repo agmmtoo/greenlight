e2dedd73b9b0770fcf8851d9c26ff0bce65a8a9e
ammo <solidifyarmor@gmail.com>
2023-06-23 12:14:30 +0630

    feat:password reset

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -23,10 +23,12 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/users/password", app.updateUserPassword)
 
 	router.Handler(http.MethodGet, "/debug/vars", expvar.Handler())
 
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/password-reset", app.createPasswordResetTokenHandler)
 
 	return app.metrics(app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router)))))
 }
